ef81b547e95de2270dd6c48e58ca9296984b6ed1
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-12 23:14:50 +0530

    modeling the database

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const cookieparser = require("cookie-parser");
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("hey");
+});
+
+app.listen(3000);
