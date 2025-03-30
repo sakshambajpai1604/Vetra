cd4d2afc18420735d593742c2a096a69e14f32e9
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-31 00:49:03 +0530

    Using multer

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../config/multer-config");
+const productModel = require("../models/product-model");
 
-router.get("/", function (req, res) {
-    res.send("hey it's working");
+router.post("/create", upload.single("image"), async function (req, res) {
+    let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+
+    let product = await productModel.create({
+        image: req.file.buffer,
+        name,
+        price,
+        discount,
+        bgcolor,
+        panelcolor,
+        textcolor,
+    });
+
+    res.send(product);
 });
 
 module.exports = router;
