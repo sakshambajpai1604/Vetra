136c9dca076df8cd2b9e8f02e318292e67e5716a
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-31 01:28:17 +0530

    Created Proucts Model

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const isloggedin = require("../middlewares/isLoggedIn");
+const productModel = require("../models/productModel");
 
 router.get("/", function (req, res) {
     let error = req.flash("error");
     res.render("index", { error });
 });
 
-router.get("/shop", isloggedin, function (req, res) {
-    res.render("shop");
+router.get("/shop", isloggedin, async function (req, res) {
+    let products = await productModel.find();
+    res.render("shop", { products });
 });
 
 router.get("/logout", isloggedin, function (req, res) {
