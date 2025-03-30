5355cbb2a77d7d9692a04fa9ada19ec3bef575d9
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-30 15:06:48 +0530

    Created isLoggedin Middleware

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const express = require("express");
 const app = express();
 const cookieparser = require("cookie-parser");
 const path = require("path");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+require("dotenv").config();
+
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
 const indexRouter = require("./routes/index");
-
-require("dotenv").config();
-
 
 const db = require("./config/mongose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
@@ -22,9 +32,5 @@ app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
-
-app.get("/", (req, res) => {
-  res.render("hey");
-});
 
 app.listen(3000);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
+const isloggedin = require("../middlewares/isLoggedIn");
+const {
+    registerUser,
+    loginUser,
+    logout,
+} = require("../controllers/authController");
 
 router.get("/", function (req, res) {
     res.send("hey it's working");
@@ -9,5 +14,7 @@ router.get("/", function (req, res) {
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+router.get("/logout", logout);
 
 module.exports = router;
