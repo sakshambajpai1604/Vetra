d8f2d42b72430d71d5c8dc3e864f9302f0d09b6b
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-04-14 13:03:09 +0530

    adding to cart

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,25 @@ const express = require("express");
 const router = express.Router();
 const isloggedin = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", function (req, res) {
     let error = req.flash("error");
-    res.render("index", { error });
+    res.render("index", { error, loggedin: false });
 });
 
 router.get("/shop", isloggedin, async function (req, res) {
     let products = await productModel.find();
-    res.render("shop", { products });
+    let success = req.flash("success");
+    res.render("shop", { products, success });
+});
+
+router.get("/addtocart/:id", isloggedin, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email })
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Added to cart");
+    res.redirect("/shop");
 });
 
 router.get("/logout", isloggedin, function (req, res) {
